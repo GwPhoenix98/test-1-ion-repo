957c6f9c93b54e1e6c055189ec7b044ef89e658d
Ion Schimbator <ionschimbator@gmail.com>
2025-11-09 14:15:04 +0200

    Add Exercitul 16

---
+++ src/Exercitiul16.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Exercitiul16 {
+    public static void main(String[] args) {
+
+        int[] numere   = {1, 2, 3, 4, 5, 6};
+        int primIndice = 0;
+        int ultIndice  = numere.length - 1;
+        int lungime    = numere.length;
+
+        System.out.println("Primul indice este: " + primIndice);
+        System.out.println("Ultimu indice este: " + ultIndice);
+        System.out.println("Lungimea array-ului este: " + lungime);
+
+    }
+
+}
