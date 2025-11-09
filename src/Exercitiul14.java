4dcee3939208bc1ddc5f552065e78409c5cd6087
Ion Schimbator <ionschimbator@gmail.com>
2025-11-09 13:55:25 +0200

    Add Exercitul 14

---
+++ src/Exercitiul14.java
@@ -0,0 +1,11 @@
+public class Exercitiul14 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            if ( i == 14 ) {
+                System.out.println("Aici eau o gustare :D");
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
