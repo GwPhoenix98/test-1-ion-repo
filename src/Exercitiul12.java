9ff596a5395fdbfcdf2a5f80c5942d790da901b3
Ion Schimbator <ionschimbator@gmail.com>
2025-11-09 13:49:50 +0200

    Add Exercitul 12

---
+++ src/Exercitiul12.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercitiul12 {
+    public static void main(String[] args) {
+
+        Scanner obScan = new Scanner(System.in);
+
+        System.out.print("Introduceti virsta persoanei: ");
+        int virsta = obScan.nextInt();
+
+        if ( virsta == 0 || virsta > 135) {
+            System.out.println("Hai las-o mai moale :D");
+        } else if ( virsta >= 1 && virsta < 12 ) {
+            System.out.println("Este copil!");
+        } else if ( virsta >= 12 && virsta <= 18) {
+            System.out.println("Este adolescent!");
+        } else if ( virsta > 18 && virsta <= 135) {
+            System.out.println("Este adult!");
+        }
+
+    }
+}
