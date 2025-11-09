0df8be3a2feeb53c9a4443be872948a69c80d1cb
Ion Schimbator <ionschimbator@gmail.com>
2025-11-09 13:28:50 +0200

    Add Exercitul 10

---
+++ src/Exercitiul10.java
@@ -0,0 +1,8 @@
+public class Exercitiul10 {
+    public static void main(String[] args) {
+
+        String unSir = "eu astazi scriu examen";
+
+        System.out.println(unSir.toUpperCase());
+    }
+}
